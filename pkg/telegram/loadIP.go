5c595e5934371a7d5ea2415867c0b8b36c91599c
Nullator <diver.rus@gmail.com>
2022-10-19 20:39:57 +0400

    Эксперименты с горутинами и каналами

--- pkg/telegram/loadIP.go
+++ pkg/telegram/loadIP.go
@@ -12,41 +12,61 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+var wg sync.WaitGroup
+
 func (b *Bot) LoadIP() {
+	c := make(chan string, len(b.config.IP))
 	for {
-		var wg sync.WaitGroup
-		wg.Add(len(b.config.IP))
 		start_time := time.Now().UnixMilli()
+		wg.Add(len(b.config.IP))
 		for ip := range b.config.IP {
-			go func(ip string) {
-				ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-				defer cancel()
-				new_report, err := parser.Start(ctx, b.config.IP[ip].First_entry, ip)
-				if err != nil {
-					log.Printf("Ошибка парсинга: %s", err)
-				}
-				old_report, err := b.base.Get(ip, ip)
-				if err != nil {
-					log.Printf("Ошибка чтения из БД при парсинге новости: %s", err)
-				}
-				if (new_report != old_report) && (new_report != "ERROR") {
-					log.Printf("Обнаружена новая запись ИП %s: %s", b.config.IP[ip].Name, new_report)
-					b.make_notify(ip, new_report)
-					err = b.base.Save(ip, new_report, ip)
-					if err != nil {
-						log.Printf("Ошибка сохранения в БД новой новости по ИП: %s", err)
-					}
-				}
-				wg.Done()
-			}(ip)
+			c <- ip
+		}
+
+		ctx, cancel := context.WithCancel(context.Background())
+		for i := 0; i < 10; i++ {
+			go b.startParse(ctx, c)
 		}
 
 		wg.Wait()
+		cancel()
 		end_time := time.Now().UnixMilli()
 		delta := end_time - start_time
 		log.Printf("Выполнен парсинг сайта МЭ за время %v милисекунд", delta)
 		time.Sleep(time.Minute * 10)
 	}
+}
+
+func (b *Bot) startParse(ctx_c context.Context, c chan string) {
+	for {
+		select {
+		case <-ctx_c.Done():
+			log.Println("Получена комманда на закрытие горутины")
+			return
+		default:
+			ip := <-c
+			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+			defer cancel()
+			new_report, err := parser.Start(ctx, b.config.IP[ip].First_entry, ip)
+			if err != nil {
+				log.Printf("Ошибка парсинга: %s", err)
+			}
+			old_report, err := b.base.Get(ip, ip)
+			if err != nil {
+				log.Printf("Ошибка чтения из БД при парсинге новости: %s", err)
+			}
+			if (new_report != old_report) && (new_report != "ERROR") {
+				log.Printf("Обнаружена новая запись ИП %s: %s", b.config.IP[ip].Name, new_report)
+				b.make_notify(ip, new_report)
+				err = b.base.Save(ip, new_report, ip)
+				if err != nil {
+					log.Printf("Ошибка сохранения в БД новой новости по ИП: %s", err)
+				}
+			}
+			wg.Done()
+		}
+	}
+
 }
 
 func (b *Bot) make_notify(ip string, news string) {
