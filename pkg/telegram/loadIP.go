e429632743f205a396bace3a20062c3c9f9e3ff0
nullator <diver.rus@gmail.com>
2024-01-31 19:24:39 +0400

    Refactor IP parsing and error handling

--- pkg/telegram/loadIP.go
+++ pkg/telegram/loadIP.go
@@ -54,8 +54,9 @@ func (b *Bot) LoadIP(ctx context.Context) {
 					slog.String("error", err.Error()))
 			}
 
+			format_delta := fmt.Sprintf("%v сек.", float64(delta)/1000)
 			slog.Info("Выполнен парсинг сайта МЭ",
-				slog.String("time", fmt.Sprintf("%v", delta)),
+				slog.String("time", fmt.Sprintf("%v", format_delta)),
 				slog.String("count", fmt.Sprintf("%v", count)))
 
 			// sleep 20 min
@@ -109,71 +110,77 @@ func (b *Bot) startParse_v2(ctx context.Context, c chan string) {
 			return
 		default:
 			ip := <-c
-			ctx_to, cancel := context.WithTimeout(ctx, 10*time.Second)
-			defer cancel()
+			func(ip string) {
+				defer wg.Done()
 
-			code := b.config.IP[ip].Code
-			data, err := parser.GetIP(ctx_to, code)
-			last_report := data[0]
+				ctx_to, cancel := context.WithTimeout(ctx, 10*time.Second)
+				defer cancel()
 
-			new_report := data[0].Dsc
-			new_report = html.UnescapeString(new_report)
-			if err != nil {
-				slog.Warn("Не удалось получить данные по api", slog.String("error", err.Error()))
-			}
+				code := b.config.IP[ip].Code
+				data, err := parser.GetIP(ctx_to, code)
+				if len(data) == 0 {
+					slog.Warn("Получена пустая запись ИП", slog.String("ИП", b.config.IP[ip].Name))
+					return
+				}
+				last_report := data[0]
 
-			// Проверяем изменилось ли количество записей ИП (для отладки)
-			old_count, err := b.base.Get(ip, "count")
-			if err != nil {
-				slog.Error("Ошибка чтения из БД при парсинге новости",
-					slog.String("error", err.Error()))
-			}
-			new_count := strconv.Itoa(len(data))
-			if new_count != old_count {
-				slog.Warn("Обнаружено изменение количества записей ИП",
-					slog.String("ip", b.config.IP[ip].Name),
-					slog.String("old_count", old_count),
-					slog.String("new_count", new_count))
-
-				err = b.base.Save(ip, new_count, "count")
+				new_report := data[0].Dsc
+				new_report = html.UnescapeString(new_report)
 				if err != nil {
-					slog.Error("Ошибка сохранения в БД нового количества записей ИП",
+					slog.Warn("Не удалось получить данные по api", slog.String("error", err.Error()))
+				}
+
+				// Проверяем изменилось ли количество записей ИП (для отладки)
+				old_count, err := b.base.Get(ip, "count")
+				if err != nil {
+					slog.Error("Ошибка чтения из БД при парсинге новости",
 						slog.String("error", err.Error()))
 				}
-			}
+				new_count := strconv.Itoa(len(data))
+				if new_count != old_count {
+					slog.Warn("Обнаружено изменение количества записей ИП",
+						slog.String("ip", b.config.IP[ip].Name),
+						slog.String("old_count", old_count),
+						slog.String("new_count", new_count))
 
-			count_str, err := b.base.Get("all", "count")
-			if err != nil {
-				slog.Error("Ошибка чтения из БД счетчика записей ИП", slog.String("error", err.Error()))
-			}
-			count, err := strconv.Atoi(count_str)
-			if err != nil {
-				slog.Error("Ошибка преобразования счетчика записей ИП", slog.String("error", err.Error()))
-			}
-			count += len(data)
-			count_str = strconv.Itoa(count)
-			err = b.base.Save("all", count_str, "count")
-			if err != nil {
-				slog.Error("Ошибка сохранения в БД счетчика записей ИП", slog.String("error", err.Error()))
-			}
-
-			old_report, err := b.base.Get(ip, ip)
-			if err != nil {
-				slog.Error("Ошибка чтения из БД старой записи по ИП", slog.String("error", err.Error()))
-			}
-
-			if (new_report != old_report) && (new_report != "ERROR") {
-				slog.Info("Обнаружена новая запись ИП",
-					slog.String("ip", b.config.IP[ip].Name),
-					slog.String("new_report", new_report))
-				b.make_notify(ip, b.config.IP[ip].Name, new_report, last_report.Src)
-				err = b.base.Save(ip, new_report, ip)
-				if err != nil {
-					slog.Error("Ошибка сохранения в БД новой новости по ИП", slog.String("error", err.Error()))
+					err = b.base.Save(ip, new_count, "count")
+					if err != nil {
+						slog.Error("Ошибка сохранения в БД нового количества записей ИП",
+							slog.String("error", err.Error()))
+					}
 				}
-			}
 
-			wg.Done()
+				count_str, err := b.base.Get("all", "count")
+				if err != nil {
+					slog.Error("Ошибка чтения из БД счетчика записей ИП", slog.String("error", err.Error()))
+				}
+				count, err := strconv.Atoi(count_str)
+				if err != nil {
+					slog.Error("Ошибка преобразования счетчика записей ИП", slog.String("error", err.Error()))
+				}
+				count += len(data)
+				count_str = strconv.Itoa(count)
+				err = b.base.Save("all", count_str, "count")
+				if err != nil {
+					slog.Error("Ошибка сохранения в БД счетчика записей ИП", slog.String("error", err.Error()))
+				}
+
+				old_report, err := b.base.Get(ip, ip)
+				if err != nil {
+					slog.Error("Ошибка чтения из БД старой записи по ИП", slog.String("error", err.Error()))
+				}
+
+				if (new_report != old_report) && (new_report != "ERROR") {
+					slog.Info("Обнаружена новая запись ИП",
+						slog.String("ip", b.config.IP[ip].Name),
+						slog.String("new_report", new_report))
+					b.make_notify(ip, b.config.IP[ip].Name, new_report, last_report.Src)
+					err = b.base.Save(ip, new_report, ip)
+					if err != nil {
+						slog.Error("Ошибка сохранения в БД новой новости по ИП", slog.String("error", err.Error()))
+					}
+				}
+			}(ip)
 		}
 	}
 
