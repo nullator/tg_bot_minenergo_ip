a61d47cf1ef5393b623b674470be1ae7fe30c282
nullator <diver.rus@gmail.com>
2024-02-08 22:53:35 +0400

    Удалены комментарии в функции startParse()

--- pkg/telegram/loadIP.go
+++ pkg/telegram/loadIP.go
@@ -71,37 +71,6 @@ func (b *Bot) LoadIP(ctx context.Context) {
 	}
 }
 
-// func (b *Bot) startParse(ctx_c context.Context, c chan string) {
-// 	for {
-// 		select {
-// 		case <-ctx_c.Done():
-// 			return
-// 		default:
-// 			ip := <-c
-// 			ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-// 			defer cancel()
-// 			new_report, err := parser.Start(ctx, b.config.IP[ip].First_entry, ip, b.logger)
-// 			if err != nil {
-// 				b.logger.Warnf("Не удалось распарсить страницу: %s", err.Error())
-// 			}
-// 			old_report, err := b.base.Get(ip, ip)
-// 			if err != nil {
-// 				b.logger.Errorf("Ошибка чтения из БД при парсинге новости: %s", err.Error())
-// 			}
-// 			if (new_report != old_report) && (new_report != "ERROR") {
-// 				b.logger.Infof("Обнаружена новая запись ИП %s: %s", b.config.IP[ip].Name, new_report)
-// 				b.make_notify(ip, new_report)
-// 				err = b.base.Save(ip, new_report, ip)
-// 				if err != nil {
-// 					b.logger.Errorf("Ошибка сохранения в БД новой новости по ИП: %s", err.Error())
-// 				}
-// 			}
-// 			wg.Done()
-// 		}
-// 	}
-
-// }
-
 func (b *Bot) startParse_v2(ctx context.Context, c chan string) {
 	for {
 		select {
